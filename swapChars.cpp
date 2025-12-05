26f072be45dd88978f9853106341f486715830c6
ARamos901 <136763187+ARamos901@users.noreply.github.com>
2025-12-05 18:42:40 -0500

    Add files via upload

--- swapChars.cpp
+++ swapChars.cpp
@@ -1,10 +1,8 @@
 #include <iostream>
 using namespace std;
 
-
-
-void swapChars(string stringg){
-
+string swapChars(string& stringg){
+//finds how long the string is to get the last char of the string
 int len=stringg.length();
 
 char* left=&stringg[0];
@@ -13,20 +11,29 @@ char* right=&stringg[len-1];
 while(left<=right){
     char temp;
 
+    //swaps chars
     temp=*left;
     *left=*right;
     *right=temp;
-
+    //displays the addresses being swapped
+    cout<<(void*)right<<endl;
+    cout<<(void*)left<<endl;
+    // increments up and down one (left and right)
     left++;
     right--;
 
 }
 
-cout<<stringg<<endl;
+return stringg;
 }
 
 int main(){
 
-    swapChars("Sixers");
+    string word="Fries";
+    cout<<"Word before reverse: "<<word<<endl;
+
+    cout<<"reversing and showing the memeory being swapped..."<<endl;
+    string res=swapChars(word);
+    cout<<"\nPost reversal: "<<res<<endl;
 
 }
